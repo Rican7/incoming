5fa85f64315a6704f37f9d313b39f2900b47401a
Trevor Suarez <rican7@gmail.com>
2015-02-18 03:03:21 -0500

    Interface for the actual processor

---
+++ src/Incoming/ProcessorInterface.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Incoming
+ *
+ * @author    Trevor Suarez (Rican7)
+ * @copyright (c) Trevor Suarez
+ * @link      https://github.com/Rican7/incoming
+ * @license   MIT
+ */
+
+namespace Incoming;
+
+/**
+ * ProcessorInterface
+ */
+interface ProcessorInterface
+{
+
+    /**
+     * Process our incoming input into a hydrated model
+     *
+     * @param mixed $input_data
+     * @param mixed $model
+     * @param HydratorInterface $hydrator
+     * @return mixed
+     */
+    public function process($input_data, $model, HydratorInterface $hydrator);
+}
