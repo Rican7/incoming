a93ac6ee48014baf0c9bd885a4d6a4609c90f302
Trevor Suarez <rican7@gmail.com>
2015-03-02 01:11:15 -0500

    Setting up testing (and finally setting up Composer)

---
+++ tests/bootstrap.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Incoming
+ *
+ * @author    Trevor Suarez (Rican7)
+ * @copyright (c) Trevor Suarez
+ * @link      https://github.com/Rican7/incoming
+ * @license   MIT
+ */
+
+require __DIR__ .'/../vendor/autoload.php';
