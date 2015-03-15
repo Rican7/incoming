bec305fb46276b23b2a577a368df884a7260dedb
Trevor Suarez <rican7@gmail.com>
2015-03-15 14:20:50 -0400

    New test for the `ReadOnlyException`

---
+++ tests/Incoming/Test/Structure/Exception/ReadOnlyExceptionTest.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Incoming
+ *
+ * @author    Trevor Suarez (Rican7)
+ * @copyright (c) Trevor Suarez
+ * @link      https://github.com/Rican7/incoming
+ * @license   MIT
+ */
+
+namespace Incoming\Test\Hydrator\Exception;
+
+use Exception;
+use Incoming\Structure\Exception\ReadOnlyException;
+use PHPUnit_Framework_TestCase;
+
+/**
+ * ReadOnlyExceptionTest
+ */
+class ReadOnlyExceptionTest extends PHPUnit_Framework_TestCase
+{
+
+    public function testForAttribute()
+    {
+        $name = 'test';
+
+        $exception = ReadOnlyException::forAttribute($name);
+
+        $this->assertTrue($exception instanceof Exception);
+        $this->assertTrue($exception instanceof ReadOnlyException);
+    }
+
+    public function testForAttributeWithValue()
+    {
+        $name = 'test';
+        $value = 'nah';
+
+        $exception = ReadOnlyException::forAttribute($name, $value);
+
+        $this->assertTrue($exception instanceof Exception);
+        $this->assertTrue($exception instanceof ReadOnlyException);
+    }
+
+    public function testForAttributeWithValueAndExceptionArgs()
+    {
+        $name = 'test';
+        $value = 'nah';
+        $code = 1337;
+        $previous = new Exception();
+
+        $exception = ReadOnlyException::forAttribute($name, $value, $code, $previous);
+
+        $this->assertTrue($exception instanceof Exception);
+        $this->assertTrue($exception instanceof ReadOnlyException);
+        $this->assertSame($code, $exception->getCode());
+        $this->assertSame($previous, $exception->getPrevious());
+    }
+}
