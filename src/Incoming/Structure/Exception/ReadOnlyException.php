d180f06392bc10cff8476de24f8d9b8e5d60ce9a
Trevor Suarez <rican7@gmail.com>
2015-03-15 14:17:27 -0400

    Improving the `ReadOnlyException`'s message

--- src/Incoming/Structure/Exception/ReadOnlyException.php
+++ src/Incoming/Structure/Exception/ReadOnlyException.php
@@ -11,6 +11,7 @@
 namespace Incoming\Structure\Exception;
 
 use BadMethodCallException;
+use Exception;
 
 /**
  * ReadOnlyException
@@ -27,6 +28,21 @@ class ReadOnlyException extends BadMethodCallException
      */
     const DEFAULT_MESSAGE = 'Illegal modification attempt';
 
+    /**
+     * The message extension format for providing an attribute context's info
+     *
+     * @type string
+     */
+    const MESSAGE_EXTENSION_FOR_ATTRIBUTE_FORMAT = ' for attribute `%s`';
+
+    /**
+     * The message extension format for providing an attribute's value info
+     * in addition to the the attribute's context
+     *
+     * @type string
+     */
+    const MESSAGE_EXTENSION_FOR_ATTRIBUTE_WITH_VALUE_FORMAT = ' and value `%s`';
+
 
     /**
      * Properties
@@ -36,4 +52,38 @@ class ReadOnlyException extends BadMethodCallException
      * @type string
      */
     protected $message = self::DEFAULT_MESSAGE;
+
+
+    /**
+     * Methods
+     */
+
+    /**
+     * Create an exception instance with an attribute's context
+     *
+     * @param string $name
+     * @param mixed|null $value
+     * @param int $code
+     * @param Exception|null $previous
+     * @return ReadOnlyException
+     */
+    public static function forAttribute($name, $value = null, $code = 0, Exception $previous = null)
+    {
+        $message = self::DEFAULT_MESSAGE . self::MESSAGE_EXTENSION_FOR_ATTRIBUTE_FORMAT;
+
+        if (null !== $value) {
+            $message = sprintf(
+                $message . self::MESSAGE_EXTENSION_FOR_ATTRIBUTE_WITH_VALUE_FORMAT,
+                $name,
+                var_export($value, true)
+            );
+        } else {
+            $message = sprintf(
+                $message,
+                $name
+            );
+        }
+
+        return new static($message, $code, $previous);
+    }
 }
