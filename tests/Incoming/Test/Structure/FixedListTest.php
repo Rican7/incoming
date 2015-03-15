c7bd9faa2ce9b819e0d86f9ccc2759d3fe933353
Trevor Suarez <rican7@gmail.com>
2015-03-15 01:57:56 -0400

    Adding tests for the two data structures

---
+++ tests/Incoming/Test/Structure/FixedListTest.php
@@ -0,0 +1,169 @@
+<?php
+/**
+ * Incoming
+ *
+ * @author    Trevor Suarez (Rican7)
+ * @copyright (c) Trevor Suarez
+ * @link      https://github.com/Rican7/incoming
+ * @license   MIT
+ */
+
+namespace Incoming\Test\Structure;
+
+use ArrayIterator;
+use Incoming\Structure\FixedList;
+use Iterator;
+use PHPUnit_Framework_TestCase;
+
+/**
+ * FixedListTest
+ */
+class FixedListTest extends PHPUnit_Framework_TestCase
+{
+
+    /**
+     * Helpers
+     */
+
+    private function getTestArrayData()
+    {
+        return [
+            'nah',
+            'sick',
+        ];
+    }
+
+    private function getTestDataNonExistentIndex()
+    {
+        return 9999999;
+    }
+
+
+    /**
+     * Tests
+     */
+
+    public function testFromArray()
+    {
+        $fixed_list = FixedList::fromArray($this->getTestArrayData());
+
+        $this->assertTrue($fixed_list instanceof FixedList);
+    }
+
+    public function testFromTraversable()
+    {
+        $fixed_list = FixedList::fromTraversable(
+            new ArrayIterator($this->getTestArrayData())
+        );
+
+        $this->assertTrue($fixed_list instanceof FixedList);
+    }
+
+    public function testGet()
+    {
+        $test_data = $this->getTestArrayData();
+        $test_valid_index = key($test_data);
+        $test_invalid_index = $this->getTestDataNonExistentIndex();
+        $default_value = 'wat';
+
+        $fixed_list = FixedList::fromArray($test_data);
+
+        // Same value for valid index
+        $this->assertSame($test_data[$test_valid_index], $fixed_list->get($test_valid_index));
+
+        // Default value for invalid index
+        $this->assertSame($default_value, $fixed_list->get($test_invalid_index, $default_value));
+    }
+
+    public function testIsEmpty()
+    {
+        // Empty
+        $fixed_list = new FixedList();
+        $this->assertTrue($fixed_list->isEmpty());
+
+        // Non-empty
+        $fixed_list = FixedList::fromArray($this->getTestArrayData());
+        $this->assertFalse($fixed_list->isEmpty());
+    }
+
+    public function testToArray()
+    {
+        $test_data = $this->getTestArrayData();
+
+        $fixed_list = FixedList::fromArray($test_data);
+
+        $this->assertEquals($test_data, $fixed_list->toArray());
+    }
+
+    public function testCount()
+    {
+        // Zero
+        $fixed_list = new FixedList();
+        $this->assertCount(0, $fixed_list);
+        $this->assertSame(0, $fixed_list->count());
+
+        // Non-zero
+        $size = 5;
+        $fixed_list = new FixedList($size);
+        $this->assertCount($size, $fixed_list);
+        $this->assertSame($size, $fixed_list->count());
+    }
+
+    public function testGetIterator()
+    {
+        $test_data = $this->getTestArrayData();
+
+        $fixed_list = FixedList::fromArray($test_data);
+
+        $this->assertTrue($fixed_list->getIterator() instanceof Iterator);
+
+        foreach ($fixed_list->getIterator() as $index => $value) {
+            $this->assertSame($test_data[$index], $value);
+        }
+    }
+
+    public function testOffsetExists()
+    {
+        $test_data = $this->getTestArrayData();
+        $test_valid_index = key($test_data);
+        $test_invalid_index = $this->getTestDataNonExistentIndex();
+
+        $fixed_list = FixedList::fromArray($test_data);
+
+        // Valid index
+        $this->assertTrue($fixed_list->offsetExists($test_valid_index));
+
+        // Invalid index
+        $this->assertFalse($fixed_list->offsetExists($test_invalid_index));
+    }
+
+    public function testOffsetGet()
+    {
+        $test_data = $this->getTestArrayData();
+        $test_valid_index = key($test_data);
+
+        $fixed_list = FixedList::fromArray($test_data);
+
+        $this->assertSame($test_data[$test_valid_index], $fixed_list->offsetGet($test_valid_index));
+    }
+
+    /**
+     * @expectedException Incoming\Structure\Exception\ReadOnlyException
+     */
+    public function testOffsetSet()
+    {
+        $fixed_list = new FixedList(1);
+
+        $fixed_list->offsetSet(0, 'test');
+    }
+
+    /**
+     * @expectedException Incoming\Structure\Exception\ReadOnlyException
+     */
+    public function testOffsetUnset()
+    {
+        $fixed_list = new FixedList(1);
+
+        $fixed_list->offsetUnset(0);
+    }
+}
---
+++ tests/Incoming/Test/Structure/MapTest.php
@@ -0,0 +1,254 @@
+<?php
+/**
+ * Incoming
+ *
+ * @author    Trevor Suarez (Rican7)
+ * @copyright (c) Trevor Suarez
+ * @link      https://github.com/Rican7/incoming
+ * @license   MIT
+ */
+
+namespace Incoming\Test\Structure;
+
+use ArrayIterator;
+use Incoming\Structure\Map;
+use Iterator;
+use MultipleIterator;
+use PHPUnit_Framework_TestCase;
+
+/**
+ * MapTest
+ */
+class MapTest extends PHPUnit_Framework_TestCase
+{
+
+    /**
+     * Helpers
+     */
+
+    private function getTestArrayData()
+    {
+        return [
+            'sick' => 'bro',
+            'cool' => 'test',
+        ];
+    }
+
+    private function getTestDataNonExistentKey()
+    {
+        return 'non-existent-key';
+    }
+
+
+    /**
+     * Tests
+     */
+
+    public function testFromTraversable()
+    {
+        $map = Map::fromTraversable(
+            new ArrayIterator($this->getTestArrayData())
+        );
+
+        $this->assertTrue($map instanceof Map);
+    }
+
+    /**
+     * @expectedException InvalidArgumentException
+     */
+    public function testFromTraversableFailsForNonScalarKeys()
+    {
+        $test_iterator = new MultipleIterator();
+        $test_iterator->attachIterator(
+            new ArrayIterator($this->getTestArrayData())
+        );
+
+        $map = Map::fromTraversable($test_iterator);
+    }
+
+    public function testFromArray()
+    {
+        $map = Map::fromArray($this->getTestArrayData());
+
+        $this->assertTrue($map instanceof Map);
+    }
+
+    public function testExists()
+    {
+        $test_data = $this->getTestArrayData();
+        $test_valid_key = key($test_data);
+        $test_invalid_key = $this->getTestDataNonExistentKey();
+
+        $map = Map::fromArray($test_data);
+
+        // Valid key
+        $this->assertTrue($map->exists($test_valid_key));
+
+        // Invalid key
+        $this->assertFalse($map->exists($test_invalid_key));
+    }
+
+    public function testGet()
+    {
+        $test_data = $this->getTestArrayData();
+        $test_valid_key = key($test_data);
+        $test_invalid_key = $this->getTestDataNonExistentKey();
+        $default_value = 'wat';
+
+        $map = Map::fromArray($test_data);
+
+        // Same value for valid key
+        $this->assertSame($test_data[$test_valid_key], $map->get($test_valid_key));
+        $this->assertNotNull($test_valid_key);
+
+        // Default value for invalid key
+        $this->assertSame($default_value, $map->get($test_invalid_key, $default_value));
+    }
+
+    public function testIsEmpty()
+    {
+        // Empty
+        $map = new Map();
+        $this->assertTrue($map->isEmpty());
+
+        // Non-empty
+        $map = Map::fromArray($this->getTestArrayData());
+        $this->assertFalse($map->isEmpty());
+    }
+
+    public function testKeys()
+    {
+        $test_data = $this->getTestArrayData();
+        $test_data_keys = array_keys($test_data);
+
+        $map = Map::fromArray($test_data);
+
+        $this->assertEquals($test_data_keys, $map->keys());
+    }
+
+    public function testToArray()
+    {
+        $test_data = $this->getTestArrayData();
+
+        $map = Map::fromArray($test_data);
+
+        $this->assertEquals($test_data, $map->toArray());
+    }
+
+    public function testCount()
+    {
+        // Zero
+        $map = new Map();
+        $this->assertCount(0, $map);
+        $this->assertSame(0, $map->count());
+
+        // Non-zero
+        $test_data = $this->getTestArrayData();
+        $map = Map::fromArray($test_data);
+        $this->assertCount(count($test_data), $map);
+        $this->assertSame(count($test_data), $map->count());
+    }
+
+    public function testGetIterator()
+    {
+        $test_data = $this->getTestArrayData();
+
+        $map = Map::fromArray($test_data);
+
+        $this->assertTrue($map->getIterator() instanceof Iterator);
+
+        foreach ($map->getIterator() as $key => $value) {
+            $this->assertSame($test_data[$key], $value);
+        }
+    }
+
+    public function testOffsetExists()
+    {
+        $test_data = $this->getTestArrayData();
+        $test_valid_key = key($test_data);
+        $test_invalid_key = $this->getTestDataNonExistentKey();
+
+        $map = Map::fromArray($test_data);
+
+        // Valid key
+        $this->assertTrue($map->offsetExists($test_valid_key));
+
+        // Invalid key
+        $this->assertFalse($map->offsetExists($test_invalid_key));
+    }
+
+    public function testOffsetGet()
+    {
+        $test_data = $this->getTestArrayData();
+        $test_valid_key = key($test_data);
+
+        $map = Map::fromArray($test_data);
+
+        $this->assertSame($test_data[$test_valid_key], $map->offsetGet($test_valid_key));
+    }
+
+    /**
+     * @expectedException Incoming\Structure\Exception\ReadOnlyException
+     */
+    public function testOffsetSet()
+    {
+        $map = new Map();
+
+        $map->offsetSet('some-key', 'test');
+    }
+
+    /**
+     * @expectedException Incoming\Structure\Exception\ReadOnlyException
+     */
+    public function testOffsetUnset()
+    {
+        $map = new Map();
+
+        $map->offsetUnset('some-key');
+    }
+
+    public function testMagicIsset()
+    {
+        $test_data = $this->getTestArrayData();
+        $test_valid_key = key($test_data);
+        $test_invalid_key = $this->getTestDataNonExistentKey();
+
+        $map = Map::fromArray($test_data);
+
+        // Valid key
+        $this->assertTrue(isset($map->{$test_valid_key}));
+
+        // Invalid key
+        $this->assertFalse(isset($map->{$test_invalid_key}));
+    }
+
+    public function testMagicGet()
+    {
+        $test_data = $this->getTestArrayData();
+        $test_valid_key = key($test_data);
+
+        $map = Map::fromArray($test_data);
+
+        $this->assertSame($test_data[$test_valid_key], $map->{$test_valid_key});
+    }
+
+    /**
+     * @expectedException Incoming\Structure\Exception\ReadOnlyException
+     */
+    public function testMagicSet()
+    {
+        $map = new Map();
+
+        $map->key = 'test';
+    }
+
+    /**
+     * @expectedException Incoming\Structure\Exception\ReadOnlyException
+     */
+    public function testMagicUnset()
+    {
+        $map = new Map();
+
+        unset($map->key);
+    }
+}
