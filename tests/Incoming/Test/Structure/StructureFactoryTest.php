2179e85db09afe73493f013c659237a2260728f4
Trevor Suarez <rican7@gmail.com>
2015-03-15 02:13:15 -0400

    Expanded the tests for the `StructureFactory`

--- tests/Incoming/Test/Structure/StructureFactoryTest.php
+++ tests/Incoming/Test/Structure/StructureFactoryTest.php
@@ -10,9 +10,11 @@
 
 namespace Incoming\Test\Structure;
 
+use ArrayIterator;
 use Incoming\Structure\FixedList;
 use Incoming\Structure\Map;
 use Incoming\Structure\StructureFactory;
+use Incoming\Structure\StructureInterface;
 use PHPUnit_Framework_TestCase;
 
 /**
@@ -21,20 +23,29 @@ use PHPUnit_Framework_TestCase;
 class StructureFactoryTest extends PHPUnit_Framework_TestCase
 {
 
-    public function testBuild()
+    /**
+     * Helpers
+     */
+
+    private function getTestMapArrayData()
     {
-        $data = [
-            [
-                'name' => 'doge',
-                'type' => 'animal',
-                'stuff' => [
-                    'woof',
-                    'wow',
-                    'very',
-                    'such',
-                    'one of these days I\'ll stop using "doge" in my code tests...',
-                ],
+        return [
+            'name' => 'doge',
+            'type' => 'animal',
+            'stuff' => [
+                'woof',
+                'wow',
+                'very',
+                'such',
+                'one of these days I\'ll stop using "doge" in my code tests...',
             ],
+        ];
+    }
+
+    private function getTestListArrayData()
+    {
+        return [
+            $this->getTestMapArrayData(),
             [
                 'name' => 'cat',
                 'type' => 'ninja',
@@ -45,6 +56,16 @@ class StructureFactoryTest extends PHPUnit_Framework_TestCase
                 ],
             ],
         ];
+    }
+
+
+    /**
+     * Tests
+     */
+
+    public function testBuildWithArray()
+    {
+        $data = $this->getTestListArrayData();
 
         $structure = StructureFactory::build($data);
 
@@ -52,5 +73,60 @@ class StructureFactoryTest extends PHPUnit_Framework_TestCase
         $this->assertTrue($structure instanceof FixedList);
         $this->assertContainsOnlyInstancesOf('Incoming\Structure\Map', $structure);
         $this->assertTrue($structure[0]['stuff'] instanceof FixedList);
+    }
+
+    public function testBuildWithTraversable()
+    {
+        $data = $this->getTestMapArrayData();
+
+        $structure = StructureFactory::build(new ArrayIterator($data));
+
+        // Assert that our types translated correctly
+        $this->assertTrue($structure instanceof Map);
+        $this->assertTrue($structure['stuff'] instanceof FixedList);
+    }
+
+    public function testBuildFromArray()
+    {
+        $data = $this->getTestListArrayData();
+
+        $structure = StructureFactory::buildFromArray($data);
+
+        $this->assertTrue($structure instanceof StructureInterface);
+    }
+
+    public function testBuildFromTraversable()
+    {
+        $data = $this->getTestListArrayData();
+
+        $structure = StructureFactory::buildFromTraversable(new ArrayIterator($data));
+
+        $this->assertTrue($structure instanceof StructureInterface);
+    }
+
+    public function testBuildWithNestedArray()
+    {
+        $data = [[[], [], []], [], []];
+
+        $structure = StructureFactory::build($data);
+
+        $this->assertTrue($structure instanceof StructureInterface);
+    }
+
+    public function testBuildWithNestedIterators()
+    {
+        $data = new ArrayIterator([
+            new ArrayIterator([
+                new ArrayIterator([]),
+                new ArrayIterator([]),
+                new ArrayIterator([]),
+            ]),
+            new ArrayIterator([]),
+            new ArrayIterator([]),
+        ]);
+
+        $structure = StructureFactory::build($data);
+
+        $this->assertTrue($structure instanceof StructureInterface);
     }
 }
