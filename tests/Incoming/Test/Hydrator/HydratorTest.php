e1789414a70c817ab2d03f410dc0a3ec4f57c0a2
Trevor Suarez <rican7@gmail.com>
2015-03-25 18:49:12 -0400

    New test for using the hydrator interface directly

---
+++ tests/Incoming/Test/Hydrator/HydratorTest.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * Incoming
+ *
+ * @author    Trevor Suarez (Rican7)
+ * @copyright (c) Trevor Suarez
+ * @link      https://github.com/Rican7/incoming
+ * @license   MIT
+ */
+
+namespace Incoming\Test\Hydrator;
+
+use PHPUnit_Framework_TestCase;
+use stdClass;
+
+/**
+ * HydratorTest
+ */
+class HydratorTest extends PHPUnit_Framework_TestCase
+{
+
+    /**
+     * Helpers
+     */
+
+    private function getMockHydratorForStdClass(array $data, stdClass $instance)
+    {
+        $mock = $this->getMockBuilder('Incoming\Hydrator\HydratorInterface')
+            ->getMock();
+
+        foreach ($data as $key => $value) {
+            $instance->{$key} = $value;
+        }
+
+        $mock->expects($this->any())
+            ->method('hydrate')
+            ->will($this->returnValue($instance));
+
+        return $mock;
+    }
+
+
+    /**
+     * Tests
+     */
+
+    public function testHydrate()
+    {
+        $test_input_data = [
+            'prop_1' => 'thing',
+            'prop_2' => 'stuff',
+            'misc' => 'yea, sure',
+        ];
+        $test_model = new stdClass();
+        $test_hydrator = $this->getMockHydratorForStdClass($test_input_data, $test_model);
+
+        $hydrated = $test_hydrator->hydrate($test_input_data, $test_model);
+
+        $this->assertSame($test_model, $hydrated);
+        $this->assertSame($test_input_data['misc'], $hydrated->misc);
+    }
+
+    public function testHydrateWithEmptyInputData()
+    {
+        $test_input_data = [];
+        $test_model = new stdClass();
+        $test_hydrator = $this->getMockHydratorForStdClass($test_input_data, $test_model);
+
+        $hydrated = $test_hydrator->hydrate($test_input_data, $test_model);
+
+        $this->assertSame($test_model, $hydrated);
+        $this->assertEmpty((array) $hydrated);
+    }
+}
