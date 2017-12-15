ca14b9673ee80c072f8bc27ef6b239d8e70163ce
Trevor Suarez <rican7@gmail.com>
2017-12-14 22:23:21 -0700

    Better type docs

--- src/Incoming/Processor.php
+++ src/Incoming/Processor.php
@@ -220,10 +220,10 @@ class Processor implements ModelProcessor, TypeProcessor
      *
      * @param mixed $input_data The input data
      * @param string $type The type to build
-     * @param Builder $builder The builder to use in the process
-     * @param Hydrator $hydrator An optional hydrator to use in the process,
-     *  after the type is built, to aid in the full hydration of the resulting
-     *  model
+     * @param Builder|null $builder The builder to use in the process
+     * @param Hydrator|null $hydrator An optional hydrator to use in the
+     *  process, after the type is built, to aid in the full hydration of the
+     *  resulting model
      * @return mixed The built model
      */
     public function processForType($input_data, string $type, Builder $builder = null, Hydrator $hydrator = null)
